f412c1d0a2edbdde413508ae826155984343f617
nhoxthang_A55 <tncthng347@gmail.com>
2024-12-02 19:20:04 +0700

    fix incorrect request method of user google login

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -7,7 +7,7 @@ const user = Router();
 user.post('/login', (req, res) => UserController.login(req, res));
 user.post('/createUser', (req, res) => UserController.createUser(req, res));
 user.get('/verify/:token', (req, res) => UserController.validateUser(req, res));
-user.get('/google', (req, res) => UserController.googleLogin(req, res));
+user.post('/google', (req, res) => UserController.googleLogin(req, res));
 user.get('/info', authenticateJWT, (req, res) => UserController.info(req, res));
 
 export default user;
