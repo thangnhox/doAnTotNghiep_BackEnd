ae2378aa4de5b06b5d89e74b55020eaf77966130
thangnhox-termux <tncthng347@gmail.com>
2024-12-06 13:13:37 +0700

    Fix typo

--- src/util/momo.ts
+++ src/util/momo.ts
@@ -1,14 +1,14 @@
-import crypto from from "crypto";
+import crypto from "crypto";
 
 export function verifySignature(data: any): boolean {
 	const { partnerCode, orderId, requestId, amount, orderInfo, orderType, partnerClientId, callbackToken, transId, resultCode, message, payType, responseTime, extraData, signature } = data;
 
-	const secret = process.env.MOMO_SECRET;
-	const access = process.env.MOMO_ACCESS;
+	const secret = process.env.MOMO_SECRET as string;
+	const access = process.env.MOMO_ACCESS as string;
 
 	const rawData = `accessKey=${access}&amount=${amount}&callbackToken=${callbackToken}&extraData=${extraData}&message=${message}&orderId=${orderId}&orderInfo=${orderInfo}&orderType=${orderType}&partnerClientId=${partnerClientId}&partnerCode=${partnerCode}&payType=${payType}&requestId=${requestId}&responseTime=${responseTime}&resultCode=${resultCode}&transId=${transId}`;
 
-	const generatedSignature = crypto.createHmac('sha256', secretKey).update(rawData).digest('hex');
+	const generatedSignature = crypto.createHmac('sha256', secret).update(rawData).digest('hex');
 
 	return generatedSignature === signature;
 }
