389468044503e8632588bb3bda8571af894bb258
nhoxthang_homer-server <tncthng347@gmail.com>
2024-12-26 16:08:50 +0700

    Remove cron test job.

--- src/services/cron.ts
+++ src/services/cron.ts
@@ -7,12 +7,10 @@ export function initCron(): void {
 
     cron.schedule('0 0 * * *', async () => {
         console.log("Start daily auto renew membership:", (new Date()).toISOString().split('T')[0]);
+        Logger.getInstance().info("Start daily auto renew membership:", (new Date()).toISOString().split('T')[0]);
         await MembershipController.autoRenewMembership();
         console.log("Daily auto renew membership done");
-    })
-
-    cron.schedule('* * * * *', () => {
-        Logger.getInstance().info("This is cron test each minute");
+        Logger.getInstance().info("Daily auto renew membership done");
     })
     
 }
